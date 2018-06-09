f3b3bae8e9002fec40bd639ac7c2eda433c44358
Theriley106 <christopherlambert106@gmail.com>
2018-06-09 17:19:42 -0400

    Revised the commit count grab

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 import requests
 import datetime
 import bs4
+import re
 
 
 def extractDate(element):
 	return str(element).partition('data-date="')[2].partition('"')[0]
 
-def extractCommitCount(element):
+def extractTotalCommitCount(element):
 	return int(str(element).partition('data-count="')[2].partition('"')[0])
 
+def getLastCommits(pageHTML, dayCount):
+	return [int(commit) for commit in re.findall('data-count="(\d+)', pageHTML)[dayCount * -1:]]
+
 def grabCommits(user, since=None):
+	commitInfo = {}
 	totalCommits = 0
-	res = requests.get("https://github.com/users/{}/contributions".format(user))
-	page = bs4.BeautifulSoup(res.text, 'lxml')
-	for val in page.select(".day"):
-		if since != None:
-			if extractDate(val) > since:
-				totalCommits = totalCommits + extractCommitCount(val)
-		else:
-			totalCommits = totalCommits + extractCommitCount(val)
-	return totalCommits
-
-
+	urlVal = "https://github.com/users/{}/contributions".format(user)
+	print urlVal
+	res = requests.get(urlVal)
+	commitInfo['year'] = sum(getLastCommits(res.text, 365))
+	commitInfo['week'] = sum(getLastCommits(res.text, 7))
+	commitInfo['day'] = sum(getLastCommits(res.text, 7))
+	return commitInfo
 
 def getTodayDate():
 	return datetime.datetime.now().strftime("%Y-%m-%d")
 
-#print grabCommits('theriley106', '2017-07-24')
+print grabCommits('theriley106', '2017-06-09')
