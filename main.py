2ab2e9b9920e5e66d88ab84243cb0e9a8758ac4e
Theriley106 <christopherlambert106@gmail.com>
2018-06-09 17:21:16 -0400

    Fixed main.py with today date

--- main.py
+++ main.py
@@ -27,4 +27,4 @@ def grabCommits(user, since=None):
 def getTodayDate():
 	return datetime.datetime.now().strftime("%Y-%m-%d")
 
-print grabCommits('theriley106', '2017-06-09')
+print grabCommits('theriley106', getTodayDate())
